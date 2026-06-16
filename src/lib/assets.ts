515571e1eedf5de71fde38de1dd8beda8d4cb159
unknown <anjalijha2k3@gmail.com>
2026-06-16 09:37:51 +0530

    Revert "feat(hero): introduce intelligence graph visualization"
    This reverts commit 1cf15dbe07dba9903cc5574959d59e0c20c05032.

--- src/lib/assets.ts
+++ src/lib/assets.ts
@@ -1,4 +1,4 @@
-import heroOrchestration from "@/assets/hero-orchestration-v4.png";
+import heroOrchestration from "@/assets/hero-orchestration-v3.jpg";
 
 export const heroCloudsSrc = heroOrchestration;
 export const logoWhiteSrc = "/images/conduence-logo-white.png";
